5849158f5323b54d37fe7ca775f78b5cdcf1ca02
Johnkey00 <1176023870@qq.com>
2025-03-09 14:56:24 -0700

    Game win modified

--- Assets/Scripts/GameWin.cs
+++ Assets/Scripts/GameWin.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 
 public class GameWin : MonoBehaviour
 {
-    // 用于记录已经到达终点的玩家
-    private static HashSet<GameObject> playersFinished = new HashSet<GameObject>();
+    // 用于记录当前在终点区域内的玩家
+    private static HashSet<GameObject> playersInGoal = new HashSet<GameObject>();
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // 如果该玩家之前还没到达终点，则添加到集合中
-            if (!playersFinished.Contains(other.gameObject))
+            // 添加进入终点区域的玩家
+            if (playersInGoal.Add(other.gameObject))
             {
-                playersFinished.Add(other.gameObject);
-                Debug.Log("A player has reached the goal! Total finished: " + playersFinished.Count);
+                Debug.Log("Player entered the goal area. Count: " + playersInGoal.Count);
             }
 
-            // 检查是否两个玩家都到达终点
-            if (playersFinished.Count >= 2)
+            // 检查是否两个玩家都在终点区域
+            if (playersInGoal.Count >= 2)
             {
-                Debug.Log("Both players have reached the goal! You Win!");
+                Debug.Log("Both players are in the goal area! You Win!");
                 if (GameOverManager.instance != null)
                 {
                     GameOverManager.instance.ShowGameOver(true);
                 }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 玩家离开终点区域后，从集合中移除
+            if (playersInGoal.Remove(other.gameObject))
+            {
+                Debug.Log("Player left the goal area. Count: " + playersInGoal.Count);
             }
         }
     }
